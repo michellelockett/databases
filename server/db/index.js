977b72dfe2f83902deb6fcb5220ca5ca490c4038
Michelle Lockett <shelldog42@gmail.com>
2018-01-13 16:43:57 -0800

    Designs schema and creates table

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,23 @@
 var mysql = require('mysql');
 
+exports.connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'chat'
+});
+connection.connect();
+
+// connection.query('SELECT * FROM messages', function(err, rows, fields)
+// {
+//   if (err) throw err;
+
+//   console.log(rows[0]);
+// });
+
+// connection.end();
+
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-
-
--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,38 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  objectId INT AUTO_INCREMENT PRIMARY KEY,
+  userId INT,
+  FOREIGN KEY (userId)
+    REFERENCES users(userId),
+  messageText VARCHAR(140),
+  roomID INT,
+  FOREIGN KEY (roomID)
+    REFERENCES rooms(roomID),
+  createdAt DATE,
+  updatedAt DATE
 );
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  userId INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), createdAt DATE
+);
+
+CREATE TABLE friends (
+  userId INT,
+  friendId INT,
+  FOREIGN KEY (userId)
+    REFERENCES users(userId),
+  FOREIGN KEY (userId)
+    REFERENCES users(userId),
+);
+
+CREATE TABLE rooms (
+  roomID INT,
+  roomname VARCHAR(20)
+);
+
+user SMALLINT UNSIGNED NOT NULL REFERENCES users(id),
 
 /* Create other tables and define schemas for them here! */
 
@@ -13,5 +44,5 @@ CREATE TABLE messages (
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+ *  to create the database and the tables. */
 
